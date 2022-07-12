bf0055740de11c5331fae3d6f649eeea9ec48495
Boss Yu <cure1218@gmail.com>
2022-07-12 07:20:12 +0000

    boss

--- core.go
+++ core.go
@@ -21,6 +21,30 @@ func New(name string, db *sqlx.DB) *Prototype {
 	}
 }
 
+func (p Prototype) RestfulInsert(c *gin.Context, req model.PrototypeInterface, me model.EngineInterface) {
+	if err := c.ShouldBindWith(req, binding.JSON); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": http.StatusText(http.StatusBadRequest)})
+	} else {
+		req.Init()
+		if _, err := me.Insert(req); err != nil {
+			if mysqlErr, ok := err.(*mysql.MySQLError); ok {
+				switch mysqlErr.Number {
+				case 1062:
+					c.JSON(http.StatusConflict, gin.H{"message": http.StatusText(http.StatusConflict)})
+				case 1452:
+					c.JSON(http.StatusUnprocessableEntity, gin.H{"message": http.StatusText(http.StatusUnprocessableEntity)})
+				default:
+					c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+				}
+			} else {
+				c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			}
+		} else {
+			c.JSON(http.StatusOK, gin.H{"message": http.StatusText(http.StatusOK), "results": req})
+		}
+	}
+}
+
 func (p Prototype) RestfulUpdateByID(c *gin.Context, req interface{}, me model.EngineInterface, id string) {
 	if err := c.ShouldBindWith(req, binding.JSON); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"message": http.StatusText(http.StatusBadRequest)})
@@ -45,5 +69,17 @@ func (p Prototype) RestfulUpdateByID(c *gin.Context, req interface{}, me model.E
 		} else {
 			c.JSON(http.StatusNoContent, nil)
 		}
+	}
+}
+
+func (p Prototype) RestfulDeleteByID(c *gin.Context, me model.EngineInterface, id string) {
+	if exists, err := me.Has(id); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+	} else if !exists {
+		c.JSON(http.StatusNotFound, gin.H{"message": http.StatusText(http.StatusNotFound)})
+	} else if _, err := me.DeleteByID(id); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+	} else {
+		c.JSON(http.StatusNoContent, nil)
 	}
 }
