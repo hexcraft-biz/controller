bc1d57138ac0a5af8ddb6e893335a89a888bd0df
Boss Yu <cure1218@gmail.com>
2022-10-03 23:21:08 +0000

    boss@home

--- core.go
+++ core.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"database/sql"
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
 	"github.com/go-sql-driver/mysql"
 	"github.com/hexcraft-biz/misc/xuuid"
 	"github.com/hexcraft-biz/model"
@@ -28,47 +27,8 @@ type ReqUri struct {
 }
 
 //================================================================
-// Insert
-//================================================================
-func (p Prototype) RestfulInsert(c *gin.Context, me, meHasUri model.EngineInterface, req, hasUriPkCols interface{}) {
-	if meHasUri != nil {
-		if exists, err := meHasUri.Has(hasUriPkCols); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-			return
-		} else if !exists {
-			c.JSON(http.StatusNotFound, gin.H{"message": http.StatusText(http.StatusNotFound)})
-			return
-		}
-	}
-
-	if _, err := me.Insert(req); err != nil {
-		MysqlErrDefaultResponse(c, err, nil)
-	} else {
-		c.JSON(http.StatusCreated, gin.H{"message": http.StatusText(http.StatusCreated), "results": req})
-	}
-}
-
-//================================================================
 // Read
 //================================================================
-
-// List
-type ReqList struct {
-	Query  string `form:"q" binding:"omitempty"`
-	Offset uint64 `form:"pos" binding:"omitempty,numeric,min=0"`
-	Length uint64 `form:"len" binding:"omitempty,numeric,min=1,max=400"`
-}
-
-func (p Prototype) RestfulList(c *gin.Context, me model.EngineInterface, dest, pkCols interface{}, searchCols []string, orderby string) {
-	req := new(ReqList)
-	if err := c.ShouldBindWith(req, binding.Query); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": http.StatusText(http.StatusBadRequest)})
-	} else if err := me.List(dest, pkCols, orderby, req.Query, searchCols, model.NewPagination(req.Offset, req.Length)); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-	} else {
-		c.JSON(http.StatusOK, gin.H{"message": http.StatusText(http.StatusOK), "results": dest})
-	}
-}
 
 // Get
 func (p Prototype) RestfulGet(c *gin.Context, me model.EngineInterface, dest, pkCols interface{}) {
@@ -84,27 +44,8 @@ func (p Prototype) RestfulGet(c *gin.Context, me model.EngineInterface, dest, pk
 	}
 }
 
-func (p Prototype) RestfulGetByKey(c *gin.Context, me model.EngineInterface, dest interface{}) {
-	uri := new(ReqUri)
-	if err := c.ShouldBindUri(uri); err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"message": http.StatusText(http.StatusNotFound)})
-	} else if err := me.GetByKey(dest, uri.ID.String()); err != nil {
-		switch err {
-		case sql.ErrNoRows:
-			c.JSON(http.StatusNotFound, gin.H{"message": http.StatusText(http.StatusNotFound)})
-		default:
-			c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-		}
-	} else {
-		c.JSON(http.StatusOK, gin.H{"message": http.StatusText(http.StatusOK), "results": dest})
-	}
-}
-
-func (p Prototype) RestfulGetByHook(c *gin.Context, hook func(dest, id interface{}) error, dest interface{}) {
-	uri := new(ReqUri)
-	if err := c.ShouldBindUri(uri); err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"message": http.StatusText(http.StatusNotFound)})
-	} else if err := hook(dest, uri.ID.String()); err != nil {
+func (p Prototype) RestfulGetByKey(c *gin.Context, me model.EngineInterface, dest, key string) {
+	if err := me.GetByKey(dest, key); err != nil {
 		switch err {
 		case sql.ErrNoRows:
 			c.JSON(http.StatusNotFound, gin.H{"message": http.StatusText(http.StatusNotFound)})
